8dbdfd2f044aab08756b1009c69e7b1732f457c0
于天琪 <yutianqi@yutianqideMacBook-Pro.local>
2022-10-07 21:49:35 +0800

    feat:first

---
+++ src/components/Echarts.vue
@@ -0,0 +1,83 @@
+
+<template>
+  <div class="about" style="display:flex;width:100%;flex-wrap:wrap">
+        <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+        <div id="myChart1" :style="{width: '300px', height: '300px'}"></div>
+        <div id="myChart2" :style="{width: '300px', height: '300px'}"></div>
+        <div id="myChart3" :style="{width: '300px', height: '300px'}"></div>
+        <div id="myChart4" :style="{width: '300px', height: '300px'}"></div>
+        <div id="myChart5" :style="{width: '300px', height: '300px'}"></div>
+        <div id="myChart6" :style="{width: '300px', height: '300px',marginBottom:'200px'}"></div>
+      </div>
+</template>
+<script>
+// import echarts from 'echarts'
+let Echarts = require('echarts/lib/echarts');
+
+export default {
+  name: 'EchartsDemo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+      console.log(Echarts)
+      this.drawLine('myChart');
+      this.drawLine('myChart1');
+      this.drawLine('myChart2');
+      this.drawLine('myChart3');
+      this.drawLine('myChart4');
+      this.drawLine('myChart5');
+      this.drawLine('myChart6');
+
+  },
+  methods: {
+    drawLine(val){
+        // 基于准备好的dom，初始化echarts实例
+        console.log(Echarts)
+        let myChart = Echarts.init(document.getElementById(val))
+        console.log('123',myChart)
+        // 绘制图表
+        myChart.setOption({
+            title: { text: 'echarts' },
+            tooltip: {},
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"],
+                axisLabel: {
+                show: true,
+                  textStyle: {
+                    color: '#c3dbff',  //更改坐标轴文字颜色
+                    fontSize : 14,      //更改坐标轴文字大小
+                    backgroundColor:'red'
+                  }
+              },
+            },
+            textStyle: {
+                    color: function (value, index) {
+                        return value >= 0 ? 'green' : 'red';
+                      },
+                    },
+            yAxis: {
+              axisLabel: {
+                show: true,
+                  textStyle: {
+                    color: '#c3dbff',  //更改坐标轴文字颜色
+                    fontSize : 14,      //更改坐标轴文字大小
+                    backgroundColor:'blue',
+                    width:'200px',
+                    height:'300px'
+                  
+                  }
+              },
+            },
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="ddd">
+      <div class="print" @click="generatorImage" style="margin-bottom:20px">
+      <button>保存图片</button>
+    </div>
+    <div class="content" ref="addImage">
+      <div ref="capture" >
+           需要保存的html片段
+           <!-- <img alt="Vue logo" src="../assets/logo.png" />
+          <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+          <EchartsDemo />
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import EchartsDemo from "@/components/Echarts.vue";
+import  html2canvas  from  'html2canvas'
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    EchartsDemo
   },
+  data() {
+    return {
+     
+    }
+  },
+  mounted() {
+    // 接收domain1的数据
+    window.addEventListener('message',(e) => {
+        console.log('iframe',e.data);
+    
+        if(e.origin !== 'http://local.chanjue.top:1024')
+        return;
+    
+        // 发送消息给domain1
+        window.parent.postMessage('来自domain2的消息', e.origin);
+    }, false);
+  },
+  methods: {
+    generatorImage() {
+      html2canvas(this.$refs.capture).then(canvas => {
+        // this.$refs.addImage.append(canvas);//在下面添加canvas节点
+        let link = document.createElement("a");
+        link.href = canvas.toDataURL();//下载链接
+        link.setAttribute("download","截取的html片段.png");
+        link.style.display = "none";//a标签隐藏
+        document.body.appendChild(link);
+        link.click();
+      });
+    },
+  }
 };
 </script>
